1805673496b54efca4a91f3aaa912bbfab1ed992
poojaVijaya <109224030+poojaVijaya@users.noreply.github.com>
2022-07-17 23:34:41 +0530

    Add files via upload

---
+++ Generics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class GenericsList{
+public static void main(String[] args){
+List<String> al=new ArrayList<>();
+al.add("pooja");
+al.add("pooji");
+al.add("pooja.arithoti@gmail.com");
+al.add("Mca"); 
+for(String s:al){
+System.out.println(s);
+}
+}
+}
